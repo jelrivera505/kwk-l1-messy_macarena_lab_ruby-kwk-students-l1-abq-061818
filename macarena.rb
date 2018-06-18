a937a3ddf687945a531e63ea950505c1930f2a22
jelrivera505 <jelrivera505@gmail.com>
2018-06-18 22:04:10 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -18,7 +18,7 @@ puts "Step 3: Turn your right palm up."
 puts "Step 4: Place your right hand on your left shoulder. Turn your left palm up."
 
 # Step 5: FIXME
-puts "Step 5: Bring your right hand up and around to the back of your head."
+puts "Step 5: Turn your left palm up. Bring your right hand up and around to the back of your head."
 
 # Step 6: FIXME
 puts "Step 6: Left arm out in front of you, palm facing down."
