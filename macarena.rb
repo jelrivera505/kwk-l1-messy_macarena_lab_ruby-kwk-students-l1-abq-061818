dd0807e7802d348081388ab9522f0354956c2116
jelrivera505 <jelrivera505@gmail.com>
2018-06-18 22:09:03 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -27,4 +27,4 @@ puts "Step 6: Place your left hand on your left shoulder."
 puts "Step 7: Left arm out in front of you, palm facing down."
 
 # Step 8: FIXME
-puts "Step 8: "
+puts "Step 8: Bring your left hand up and around to the back your head."
